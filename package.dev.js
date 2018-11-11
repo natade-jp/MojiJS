ad5607756de26823e21ed1d837d481983f522804
natade-jp <natade3@gmail.com>
2018-11-11 12:56:21 +0900

    フォルダの整理

--- package.dev.js
+++ package.dev.js
@@ -2,6 +2,11 @@ const fs = require("fs");
 const input_file_name = "./src/Senko.js";
 const output_file_name = "./build/Senko.js";
 
+// ファイルを読み込む
 let es_text = fs.readFileSync(input_file_name, "utf-8");
+
+// import するファイルパスを変更
 es_text = es_text.replace(/(import\s+\w+\s+from\s+")(.\/)([^;]+;)/g, "$1../src/$3");
+
+// build用のフォルダへ保存
 fs.writeFileSync(output_file_name, es_text, "utf-8");
--- rollup.config.js
+++ rollup.config.js
@@ -4,32 +4,32 @@ import uglifyES from "rollup-plugin-uglify-es";
 
 export default [
 	{
-		input: 'src/Senko.js',
+		input: "src/Senko.js",
 		output: {
-			file: 'build/Senko.js',
-			format: 'esm'
+			file: "build/Senko.js",
+			format: "esm"
 		}
 	},
 	{
-		input: 'src/Senko.js',
+		input: "src/Senko.js",
 		plugins: [
 			uglifyES(),
 		],
 		output: {
-			file: 'build/Senko.module.js',
-			format: 'esm'
+			file: "build/Senko.module.js",
+			format: "esm"
 		}
 	},
 	{
-		input: 'src/Senko.js',
+		input: "src/Senko.js",
 		moduleName: "Senko",
 		plugins: [
 			buble(),
 			uglifyJS.uglify(),
 		],
 		output: {
-			file: 'build/Senko.umd.js',
-			format: 'umd'
+			file: "build/Senko.umd.js",
+			format: "umd"
 		}
 	}
 ];
