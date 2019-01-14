d32f44d083691f4c6a252eedce98db545ff539da
natade-jp <natade3@gmail.com>
2019-01-14 23:39:29 +0900

    SNumber 追加

---
+++ src/math/SNumber.js
@@ -0,0 +1,25 @@
+﻿/**
+ * The script is part of SenkoJS.
+ * 
+ * AUTHOR:
+ *  natade (http://twitter.com/natadea)
+ * 
+ * LICENSE:
+ *  The zlib/libpng License https://opensource.org/licenses/Zlib
+ */
+
+import Complex from "./number/Complex.js";
+import BigDecimal from "./large/BigDecimal.js";
+import BigInteger from "./large/BigInteger.js";
+import Random from "./basic/Random.js";
+
+const SNumber = {
+	
+	Complex : Complex,
+	BigDecimal : BigDecimal,
+	BigInteger : BigInteger,
+	Random : Random
+
+};
+
+export default SNumber;
